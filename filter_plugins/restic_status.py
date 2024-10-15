bf994c9cc4a6ca189550b4ef3aa814970cfa4031
lb3dev <lben3dev@gmail.com>
2024-10-15 05:44:04 -0400

    Updated custom filter plugin to capture additional information from running restic backup commands - Added files, dirs, blobs counts, if it is running in dry run, total files/bytes processed, if a snapshot is created from the custom filter plugin - In restic role, use the same verbose and json options like dry run for the actual backup command - Also capture the output from running the actual backup command and output the lines - Updated the jq filters to capture summary message types from output

--- filter_plugins/restic_status.py
+++ filter_plugins/restic_status.py
@@ -1,3 +1,5 @@
+from ansible.errors import AnsibleFilterError
+
 class FilterModule(object):
 
     def filters(self):
@@ -6,4 +8,48 @@ class FilterModule(object):
         }
 
     def restic_status(self, value):
-        return value['action'] + ': ' + value['item']
+        status = ''
+        if 'message_type' in value:
+            message_type = value['message_type']
+            if message_type == 'summary':
+
+                status += 'Files:\n'
+                status += str(value['files_new']) + ' New, '
+                status += str(value['files_changed']) + ' Changed, '
+                status += str(value['files_unmodified']) + ' Unmodified\n'
+
+                status += 'Dirs:\n'
+                status += str(value['dirs_new']) + ' New, '
+                status += str(value['dirs_changed']) + ' Changed, '
+                status += str(value['dirs_unmodified']) + ' Unmodified\n'
+
+                status += 'Blobs:\n'
+                status += str(value['data_blobs']) + ' Data Blobs, '
+                status += str(value['tree_blobs']) + ' Tree Blobs\n\n'
+
+                if 'dry_run' in value and value['dry_run']:
+                    status += 'Dry Run: Yes\n\n'
+                if not ('dry_run' in value):
+                    status += 'Dry Run: No\n\n'
+
+                status += 'Total files processed: ' + str(value['total_files_processed']) + ', '
+                status += 'Total bytes processed: ' + '{:.2f}'.format(value['total_bytes_processed']/(1024*1024)) + 'MB' + ' = ' + '{:.2f}'.format(value['total_bytes_processed']/(1024*1024*1024)) + 'GB, '
+                status += 'Total duration: ' + str(round(value['total_duration'], 1)) + 's\n'
+                if 'snapshot_id' in value:
+                    status += 'Creating snapshot id: ' + value['snapshot_id']
+                else:
+                    status += 'No snapshot id created, backup is skipped'
+
+            elif message_type == 'verbose_status':
+                if 'action' in value:
+                    status += value['action'] + ': ' + value['item']
+            elif message_type == 'status':
+                status += 'Percent done: ' + str(value['percent_done'])
+            elif message_type == 'error':
+                status += str(value)
+            else:
+                raise AnsibleFilterError(f'message_type: {message_type} not recognized')
+        else:
+            raise AnsibleFilterError(f'No message_type element found')
+
+        return status
